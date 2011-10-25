b1df573ad1ad5e8d914b386495f7e13194fd1605
Dan Tenenbaum <dtenenba@fhcrc.org>
2011-10-25 05:29:31 +0000

    onload cleanup
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/tinesath1cdf@1789 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++
@@ -1,5 +0,0 @@
-.onLoad <- function(libname, pkgname){
-	path = .path.package(pkgname)
-	where <- as.environment(match(paste("package:", pkgname, sep = ""),search()))
-	load(file.path(path, "data", "tinesath1cdf.rda"), envir = where)
-}
